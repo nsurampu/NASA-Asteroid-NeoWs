13e0ec8d1112697c785d968d710c440faed73acf
Naren Surampudi <nsurampu@gmail.com>
2024-03-26 23:44:38 +0530

    Dashboard additions and enhancements

--- Dashboard.py
+++ Dashboard.py
@@ -1,6 +1,12 @@
 import streamlit as st
 from datetime import datetime, timedelta
 from core import Core
+import pandas as pd
+# import matplotlib.pyplot as plt
+# import seaborn as sns
+# import mpld3
+# import streamlit.components.v1 as components
+import plotly.express as px
 
 
 st.set_page_config(layout='wide')
@@ -12,18 +18,44 @@ end_date = datetime.now().strftime("%Y-%m-%d")
 obj = Core()
 obj.set_metrics()
 
-st.title("Asteroids - Near Earth Object Web Service Dashboard")
-st.markdown("*A 7-day rolling window dashboard for Near Earth Objects. Source: NASA*")
+tab1, tab2 = st.tabs(["Dashboard", "Raw Data"])
 
-metric_col1, metric_col2 = st.columns(2)
+with tab1:
 
-metric_col1.metric("Near Earth Objects", obj.neos)
-metric_col2.metric("Potentially Hazardous", obj.hazard)
-metric_col1.metric("Flagged by Sentry", obj.sentry)
-metric_col2.metric("Avg H Value", obj.h_mag)
-metric_col1.metric("Min Estimated Diameter", obj.min_diam + " m")
-metric_col2.metric("Max Estimated Diameter", obj.max_diam + " m")
-metric_col1.metric("Min Miss Distance", obj.min_miss + " km")
-metric_col2.metric("Max Miss Distance", obj.max_miss + " km")
-metric_col1.metric("Min Relative Velocity", obj.min_vel + " km/s")
-metric_col2.metric("Max Relative Velocity", obj.max_vel + " km/s")
+    st.title("Asteroids - Near Earth Object Web Service Dashboard")
+    st.markdown("*A 7-day rolling window dashboard for Near Earth Objects. Source: NASA*")
+
+    metric_col1, metric_col2, metric_col3, metric_col4 = st.columns(4)
+
+    metric_col1.metric("Near Earth Objects", obj.neos)
+    metric_col2.metric("Potentially Hazardous", obj.hazard)
+    metric_col3.metric("Flagged by Sentry", obj.sentry)
+    metric_col4.metric("Avg H Value", obj.h_mag)
+    metric_col1.metric("Min Estimated Diameter", obj.min_diam + " km")
+    metric_col2.metric("Max Estimated Diameter", obj.max_diam + " km")
+    metric_col3.metric("Min Miss Distance", obj.min_miss + " au")
+    metric_col4.metric("Max Miss Distance", obj.max_miss + " au")
+    metric_col1.metric("Min Relative Velocity", obj.min_vel + " km/s")
+    metric_col2.metric("Max Relative Velocity", obj.max_vel + " km/s")
+
+    bar_col1, bar_col2 = st.columns((2, 1))
+    bar_chart_data = obj.get_bar_chart_data()
+    # bar_chart = plt.figure(figsize=(11, 5))
+    # plt.title("Near Earth Object Count Distribution")
+    # sns.barplot(data=bar_chart_data, x='Date', y='Object Count')
+    # components.html(mpld3.fig_to_html(bar_chart), height=500)
+    bar_chart = px.bar(bar_chart_data, x='Date', y='Object Count', hover_data=['Object Count', 'Potentially Hazardous', 'Sentry Objects', 'Min Estimated Diameter (km)', 'Max Estimated Diameter (km)',
+    'Min Miss Distance (au)', 'Max Miss Distance (au)', 'Min Relative Velocity (km/s)', 'Max Relative Velocity (km/s)'])
+    bar_col1.plotly_chart(bar_chart, theme='streamlit', use_container_width=True)
+
+    date_options = bar_col2.multiselect(label="Select date(s) to get observed Object IDs", options=pd.unique(bar_chart_data['Date'].values), default=pd.unique(bar_chart_data['Date'].values)[0])
+    selected_data_options = [x for x in date_options]
+    bar_col2.table(bar_chart_data.loc[bar_chart_data['Date'].isin(selected_data_options)]['Objects'])
+
+with tab2:
+
+    data_option = st.selectbox(label="Select data to view", options=['NEO 7-day data', 'NEO Close-approach data'])
+    if data_option=='NEO 7-day data':
+        st.dataframe(obj.neo_data)
+    elif data_option=='NEO Close-approach data':
+        st.dataframe(obj.neo_close_data)
--- core.py
+++ core.py
@@ -14,9 +14,9 @@ class Core:
         self.sentry = None
         self.h_mag = None
         self.min_diam = None
-        self.min_dima_obj = None
+        self.min_diam_obj = None
         self.max_diam = None
-        self.max_dima_obj = None
+        self.max_diam_obj = None
         self.hazard = None
         self.max_miss = None
         self.max_miss_obj = None
@@ -44,6 +44,8 @@ class Core:
                 data.loc[len(data.index)] = [date, r['id'], r['neo_reference_id'], r['name'], r['absolute_magnitude_h'], r['estimated_diameter']['meters']['estimated_diameter_min'],
                 r['estimated_diameter']['meters']['estimated_diameter_max'], r['is_potentially_hazardous_asteroid'], r['close_approach_data'][0]['relative_velocity']['kilometers_per_second'],
                 r['close_approach_data'][0]['miss_distance']['kilometers'], r['close_approach_data'][0]['orbiting_body'], r['is_sentry_object']]
+        data['ID'] = data['ID'].apply(lambda x: str(x))
+        data['NEO Reference ID'] = data['NEO Reference ID'].apply(lambda x: str(x))
         data.to_excel('data/neo_data.xlsx', index=False)
 
         obj_data = pd.DataFrame(columns=['ID', 'Close Approach Date', 'Relative Velocity (km/s)', 'Miss Distance (au)', 'Orbiting Body'])
@@ -52,6 +54,7 @@ class Core:
             lookup_response = json.loads(lookup_response.text)
             for closup in lookup_response['close_approach_data']:
                 obj_data.loc[len(obj_data.index)] = [id, closup['close_approach_date'], closup['relative_velocity']['kilometers_per_second'], closup['miss_distance']['astronomical'], closup['orbiting_body']]
+        obj_data['ID'] = obj_data['ID'].apply(lambda x: str(x))
         obj_data.to_excel('data/neo_close_data.xlsx', index=False)
 
 
@@ -67,3 +70,32 @@ class Core:
         self.max_miss = str(round(max(self.neo_data['Miss Distance (au)'].values), 2))
         self.min_vel = str(round(min(self.neo_data['Relative Velocity (km/s)'].values), 2))
         self.max_vel = str(round(max(self.neo_data['Relative Velocity (km/s)'].values), 2))
+
+        self.min_diam_obj = self.neo_data.loc[self.neo_data['Min Estimated Diameter (km)']==min(self.neo_data['Min Estimated Diameter (km)'].values)].values[0]
+        self.max_diam_obj = self.neo_data.loc[self.neo_data['Max Estimated Diameter (km)']==min(self.neo_data['Max Estimated Diameter (km)'].values)].values[0]
+        self.min_miss_obj = self.neo_data.loc[self.neo_data['Miss Distance (au)']==min(self.neo_data['Miss Distance (au)'].values)].values[0]
+        self.max_miss_obj = self.neo_data.loc[self.neo_data['Miss Distance (au)']==max(self.neo_data['Miss Distance (au)'].values)].values[0]
+        self.min_vel_obj = self.neo_data.loc[self.neo_data['Relative Velocity (km/s)']==min(self.neo_data['Relative Velocity (km/s)'].values)].values[0]
+        self.max_vel_obj = self.neo_data.loc[self.neo_data['Relative Velocity (km/s)']==max(self.neo_data['Relative Velocity (km/s)'].values)].values[0]
+
+
+    def get_bar_chart_data(self):
+
+        chart = pd.DataFrame(columns=['Date', 'Objects'])
+        chart['Date'] = pd.unique(self.neo_data['Date'])
+        chart['Object Count'] = chart['Date'].apply(lambda x: len(pd.unique(self.neo_data.loc[self.neo_data['Date']==x]['ID'])))
+        chart['Objects'] = chart['Date'].apply(lambda x: ', '.join(pd.unique(self.neo_data.loc[self.neo_data['Date']==x]['ID'].apply(lambda x: str(x)))))
+        chart['Min Estimated Diameter (km)'] = chart['Date'].apply(lambda x: round(min(self.neo_data.loc[self.neo_data['Date']==x]['Min Estimated Diameter (km)'].values), 2))
+        chart['Max Estimated Diameter (km)'] = chart['Date'].apply(lambda x: round(max(self.neo_data.loc[self.neo_data['Date']==x]['Max Estimated Diameter (km)'].values), 2))
+        chart['Min Miss Distance (au)'] = chart['Date'].apply(lambda x: round(min(self.neo_data.loc[self.neo_data['Date']==x]['Miss Distance (au)'].values), 2))
+        chart['Max Miss Distance (au)'] = chart['Date'].apply(lambda x: round(max(self.neo_data.loc[self.neo_data['Date']==x]['Miss Distance (au)'].values), 2))
+        chart['Min Relative Velocity (km/s)'] = chart['Date'].apply(lambda x: round(min(self.neo_data.loc[self.neo_data['Date']==x]['Relative Velocity (km/s)'].values), 2))
+        chart['Max Relative Velocity (km/s)'] = chart['Date'].apply(lambda x: round(max(self.neo_data.loc[self.neo_data['Date']==x]['Relative Velocity (km/s)'].values), 2))
+        chart['Potentially Hazardous'] = chart['Date'].apply(lambda x: len(pd.unique(self.neo_data.loc[(self.neo_data['Date']==x) & (self.neo_data['Potentially Hazardous']==True)]['ID'])))
+        chart['Sentry Objects'] = chart['Date'].apply(lambda x: len(pd.unique(self.neo_data.loc[(self.neo_data['Date']==x) & (self.neo_data['Sentry Object']==True)]['ID'])))
+
+        chart['Date'] = chart['Date'].apply(lambda x: pd.to_datetime(x))
+        chart.sort_values(by=['Date'], inplace=True)
+        chart['Date'] = chart['Date'].apply(lambda x: x.strftime("%d-%b-%y"))
+
+        return chart
